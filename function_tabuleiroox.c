46b772a0a2e3397fabf900011bfcd5f2a58e0768
mendes <ymendeszx@gmail.com>
2026-07-29 19:10:48 -0300

    Update function_tabuleiroox.c

--- function_tabuleiroox.c
+++ function_tabuleiroox.c
@@ -1,7 +1,23 @@
-#include <stdio.h>
+int validaCaractereTabuleiro(char c) 
+{
+    return (c == 'X' || c == 'O');
+}
 
-int validar_tabuleiro_char(char c) {
-    if (c == 'X') return 1;
-    if (c == 'O') return 1;
-    return 0;
+int processar(int argc, char *argv[], char tabuleiro[3][3]) {
+    if (argc < 10) {
+        return 0; 
+    }
+    
+    int k = 1;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            char c = argv[k][0];
+            if (!validaCaractereTabuleiro(c)) {
+                return -1;
+            }
+            tabuleiro[i][j] = c;
+            k++;
+        }
+    }
+    return 1;
 }
